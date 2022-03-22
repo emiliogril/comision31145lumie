85eb940e9f89dcd811b3dd2cbc67375a6a606ba3
Sebastian <szuviria@gmail.com>
2022-03-21 22:45:25 -0300

    Clase de jsx y webpack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
+  const styles = { 
+    color: 'red', 
+    fontSize: '40px'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Comision 31145</h1>
+        <NavBar />
+        <img src={'./images/logo192.png'} className="App-logo" alt="logo" />
+        <h1 style={styles} className="Titulo">Hola Comision 31145</h1>
         <button>Boton</button>
       </header>
     </div>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+        <nav className="NavBar">
+            <button>boton1</button>
+        </nav>
+    )
+}
+
+export default NavBar
