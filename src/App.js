adebd6e5e3e5c780cf6f5900677e620c26267c45
Sebastian Zuviria <szuviria@gmail.com>
2022-04-04 22:39:31 -0300

    Clase consumiendo apis

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Counter from './components/Counter/Counter';
+import MLListContainer from './components/MLListContainer/MLListContainer'
+
 
 const App = () => {
  const [show, setShow] = useState(true)
@@ -12,9 +14,10 @@ const App = () => {
 
   return (
       <div className="App">
-        <ItemListContainer greeting={'Hola coders'} />
+        {/* <ItemListContainer greeting={'Hola coders'} /> */}
         {/* <button onClick={() => setShow(!show)}>{show ? 'Desmontar contador' : 'Montar contador'}</button>
         { show ? <Counter initial={0} stock={10} onAdd={handleOnAdd}/> : null } */}
+        <MLListContainer />
       </div>
   );
 }
